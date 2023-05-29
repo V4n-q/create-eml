a439a16fd4772f64648e3e346d9cbe908a376aaa
V4n-q <veevyhee@gmail.com>
2023-05-29 15:24:01 +0545

    Method and variables naming convention changed

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ import re
 import logging
 
 # get valid email addresses using regex
-def getValidEmailAddress(prompt):
+def get_valid_email_address(prompt):
     """
     This function takes an email address prompt as input and returns a valid email address. 
     It uses a regex to check whether the email address is in a valid format or not.
     """
 
-    emailRegex = r"[^@]+@[^@]+\.[^@]+"
+    email_regex = r"[^@]+@[^@]+\.[^@]+"
     while True:
-        emailAddress = input(prompt)
-        if re.match(emailRegex, emailAddress):
-            return emailAddress
+        email_address = input(prompt)
+        if re.match(email_regex, email_address):
+            return email_address
         print("Invalid email address format. Please enter a valid email address")
 
 # add attachemnt to email message
-def addAttachment(msg):
+def add_attachment(msg):
     """
     This function takes an email message as input and adds attachments to it.
     It prompts the user to choose whether they want to attach a single file or multiple files.
@@ -30,78 +30,78 @@ def addAttachment(msg):
     print("1.Single File\n2.Multiple File")
     while True:
         try:
-            attachMode = int(input("Choose 1 or 2: "))
+            attach_mode = int(input("Choose 1 or 2: "))
             # validating user input
-            if attachMode in (1,2):
+            if attach_mode in (1,2):
                 break
         except ValueError:
             pass
         print("Invalid Input.")
     # Single File Mode
-    if attachMode == 1:
-        filePath = input("File Path: ").strip('"')
+    if attach_mode == 1:
+        file_path = input("File Path: ").strip('"')
         print("\n")
-        while(os.path.isfile(filePath) == False):
-            filePath = input("A directory path was provided instead of a file path. Please enter File Path: ").strip('"')
+        while(os.path.isfile(file_path) == False):
+            file_path = input("A directory path was provided instead of a file path. Please enter File Path: ").strip('"')
         try:
             # open file in binary mode
-            with open(filePath, 'rb') as f:
-                fileData = f.read()
-                fileName= os.path.basename(filePath)
+            with open(file_path, 'rb') as f:
+                file_data = f.read()
+                file_name= os.path.basename(file_path)
                 # Gets File extension and removes dot from the extension
-                fileExtension= os.path.splitext(fileName)[1].strip(".")
+                file_extension= os.path.splitext(file_name)[1].strip(".")
             # add file data as attachment to message
-            msg.add_attachment(fileData,maintype='application',subtype=fileExtension,filename=fileName)
+            msg.add_attachment(file_data,maintype='application',subtype=file_extension,filename=file_name)
         except (FileNotFoundError, PermissionError) as e:
             # log error and print error message
-            logging.error(f"Error: {str(e)} - {fileName}")
-            print(f"Error: {fileName} not found or permission denied.")
+            logging.error(f"Error: {str(e)} - {file_name}")
+            print(f"Error: {file_name} not found or permission denied.")
     # Multiple File Mode
-    elif attachMode == 2:
-        successFileCount = 0
-        failureFileCount = 0
+    elif attach_mode == 2:
+        success_file_count = 0
+        failure_file_count = 0
         folderPath = input("\nFolder Path: ").strip('"')
         try:
             # traverse directory structure recursively
             for root,dirs,files in os.walk(folderPath):
                 #iterate over the files in current directory
-                for fileName in files:
+                for file_name in files:
                     #construct file path
-                    filePath = os.path.join(root,fileName)
+                    file_path = os.path.join(root,file_name)
                     try:
-                        with open(filePath, 'rb') as f:
-                            fileData = f.read()
-                            fileExtension= os.path.splitext(fileName)[1]
-                        msg.add_attachment(fileData,maintype='application',subtype=fileExtension,filename=fileName)
-                        successFileCount+=1
+                        with open(file_path, 'rb') as f:
+                            file_data = f.read()
+                            file_extension= os.path.splitext(file_name)[1]
+                        msg.add_attachment(file_data,maintype='application',subtype=file_extension,filename=file_name)
+                        success_file_count+=1
                     except PermissionError:
-                        logging.error(f"Error: Permission denied {filePath}")
-                        print(f"File {fileName} not attached due to permission error.")
-                        failureFileCount += 1
+                        logging.error(f"Error: Permission denied {file_path}")
+                        print(f"File {file_name} not attached due to permission error.")
+                        failure_file_count += 1
                     except FileNotFoundError as e:
-                        logging.error(f"{filePath} not found, {fileName}")
+                        logging.error(f"{file_path} not found, {file_name}")
                         print(f"File Not Found.")
-                        failureFileCount += 1
+                        failure_file_count += 1
                     except Exception as e:
-                        logging.error(f"Error: {str(e)} - {fileName}")
+                        logging.error(f"Error: {str(e)} - {file_name}")
                         print("Error occured.Please check log.")
-                        failureFileCount += 1
+                        failure_file_count += 1
         except Exception as e:
-            logging.error(f"Error: {str(e)} - {fileName}")
+            logging.error(f"Error: {str(e)} - {file_name}")
             print("Error occured.Please check log.")
-        if successFileCount > 0:
-            print(f"{successFileCount} files attached successfully.")
-        if failureFileCount > 0:
-            print(f"{failureFileCount} files failed to attach.")
+        if success_file_count > 0:
+            print(f"{success_file_count} files attached successfully.")
+        if failure_file_count > 0:
+            print(f"{failure_file_count} files failed to attach.")
     else:
         print("Invalid Input. Please enter '1' or '2'.")
 
 if __name__=="__main__":
     # configure logging settings
-    logging.basicConfig(filename="attachment.log",level=logging.ERROR, format='%(asctime)s %(levelname)s: %(message)s')
+    logging.basicConfig(file_name="attachment.log",level=logging.ERROR, format='%(asctime)s %(levelname)s: %(message)s')
     
-    attachmentConfirmation = ["y","n","yes","no"]
-    attachmentPositiveConfirm= ["y","yes"]
+    attachment_confirmation = ["y","n","yes","no"]
+    attachment_positive_confirm= ["y","yes"]
     
     print("\n\tEML Creation\n")
 
@@ -110,8 +110,8 @@ if __name__=="__main__":
         receiver = sys.argv[2]
         subject = sys.argv[3]
     else:
-        sender = getValidEmailAddress("From: ")
-        receiver = getValidEmailAddress("To: ")
+        sender = get_valid_email_address("From: ")
+        receiver = get_valid_email_address("To: ")
         subject = input("Subject: ")
 
     #creating empty message
@@ -123,23 +123,23 @@ if __name__=="__main__":
     msg['Subject'] = subject
 
     #setting message body
-    msgBody=input("Message Body: ")
-    msg.set_content(msgBody)
+    msg_body=input("Message Body: ")
+    msg.set_content(msg_body)
 
     #attachment prompt
-    attachFile= input("\nAttach File To Email? (y/n): ").lower()
-    while attachFile not in attachmentConfirmation:
-        attachFile = input("Invalid input. Attach File To Email? (y/n): ").lower()
-    if(attachFile in attachmentPositiveConfirm):
-        addAttachment(msg)
+    attach_file= input("\nAttach File To Email? (y/n): ").lower()
+    while attach_file not in attachment_confirmation:
+        attach_file = input("Invalid input. Attach File To Email? (y/n): ").lower()
+    if(attach_file in attachment_positive_confirm):
+        add_attachment(msg)
             
     #EML File Creation
-    emlFileName=input("\nEnter the EML File Name: ")
-    fullEmlFileName = emlFileName + ".eml"
+    eml_file_name=input("\nEnter the EML File Name: ")
+    full_eml_file_name = eml_file_name + ".eml"
     try:
-        with open(fullEmlFileName, 'w') as f:
+        with open(full_eml_file_name, 'w') as f:
             f.write(str(msg))
-            print(f"'{emlFileName}' has been saved as '{fullEmlFileName}'.")
+            print(f"'{eml_file_name}' has been saved as '{full_eml_file_name}'.")
     except Exception as e:
         logging.error(f"Error: {str(e)}")
-        print(f"An error occurred while saving '{emlFileName}' as '{fullEmlFileName}' : {str(e)}")
+        print(f"An error occurred while saving '{eml_file_name}' as '{full_eml_file_name}' : {str(e)}")
